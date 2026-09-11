386e81622c30b58d6041cf1dfaf079ef1cb469d6
ayushkrsingh <ayushks070707@gmail.com>
2026-09-11 19:59:50 +0530

    Implemented ImageKit as Cloud Image service provider and created api to upload posts

--- src/app.js
+++ src/app.js
@@ -1,4 +1,25 @@
 const express = require('express');
+const multer = require('multer');
+const postModel = require('./models/post.model');
+const uploadFile = require('./services/storage.service');
+
+const upload = multer({ storage: multer.memoryStorage() });
 const app = express();
+app.use(express.json());
+
+app.post('/create-post', upload.single('image'), async (req, res) => {
+    console.log(req.body);
+    console.log(req.file);
+    const result = await uploadFile(req.file.buffer);
+    const post = await postModel.create({
+        image: result.url,
+        caption: req.body.caption
+    });
+    return res.status(201).json({
+        message: 'Post created successfully',
+        post
+    })
+});
+
 
 module.exports = app;
---
+++ src/models/post.model.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const postSchema = new mongoose.Schema({
+    image:String,
+    caption: String,
+});
+
+const postModel = mongoose.model('Post', postSchema);
+
+
+module.exports = postModel;
+
+// post{
+//      image: "path/to/image.jpg",
+//      caption: "This is a sample caption"
+// }
